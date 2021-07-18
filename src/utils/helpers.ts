e8274bcafb52aa65ee603c3933f3d7d3d21afb86
SpaceKatt <spacekattpoispin@gmail.com>
2021-07-18 16:58:05 -0700

    add utils to gzip files

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,7 +1,36 @@
+import * as pfs from 'fs/promises';
+
+import { createGzip, gzip, unzip } from 'zlib';
+import { promisify } from 'util';
+import { pipeline, Readable } from 'stream';
+import { createWriteStream } from 'fs';
+
 export const utfToHex = (s: string): string => {
     return Buffer.from(s, 'utf-8').toString('hex');
 };
 
 export const hexToUtf = (s: string): string => {
     return Buffer.from(s, 'hex').toString('utf-8');
+};
+
+const pipe = promisify(pipeline);
+
+export const writeCompressedFile = async (
+    data: any,
+    outPath: string,
+): Promise<void> => {
+    const gzip = createGzip();
+    const source = Readable.from(JSON.stringify(data));
+    const sink = createWriteStream(outPath);
+    await pipe(source, gzip, sink);
+};
+
+const do_unzip = promisify(unzip);
+
+export const readCompressedFile = async (inPath: string): Promise<any> => {
+    await pfs.access(inPath);
+    const fh = await pfs.open(inPath, 'r');
+    const data = await fh.readFile();
+    const result = await do_unzip(data);
+    return result;
 };

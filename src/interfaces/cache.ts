bdb38d0521534a0e6c7953ccc58115f0a9e8f796
SpaceKatt <spacekattpoispin@gmail.com>
2021-07-18 16:32:25 -0700

    update cache interface

--- src/interfaces/cache.ts
+++ src/interfaces/cache.ts
@@ -4,6 +4,9 @@ export enum CacheType {
 }
 
 export interface Cache {
-    get(key: string): any | undefined;
-    set(key: string, data: any): void;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    get(key: string): Promise<any | undefined>;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    set(key: string, data: any): Promise<void>;
+    flush(): void;
 }

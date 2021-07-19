980bc5bd21cec0db670baeb85cfbb934058e9cf3
SpaceKatt <spacekattpoispin@gmail.com>
2021-07-18 17:03:18 -0700

    write sample main

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('Hello, World!');
+import { CacheFactory, CacheFactoryOpts } from './factories';
+import { CacheType } from './interfaces';
+
+async function main() {
+    const cacheFactoryOpts: CacheFactoryOpts = {
+        type: CacheType.FILE,
+        name: 'github',
+    };
+    const cache = CacheFactory.getInstance(cacheFactoryOpts);
+
+    const sample_key = 'reeee';
+    const sample = { ree: 'cola' };
+
+    await cache.set(sample_key, sample);
+
+    const ree = await cache.get(sample_key);
+    console.log(ree);
+}
+
+main();

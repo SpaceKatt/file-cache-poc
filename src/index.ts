c1bb0a06d1e3f58e4f83c662b0b5aeb68941f7a6
SpaceKatt <spacekattpoispin@gmail.com>
2021-07-18 20:54:20 -0700

    cleanup main

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,11 @@
-import { CacheFactory, CacheFactoryOpts } from './factories';
 import { CacheType } from './interfaces';
+import { GitHubService } from './services';
 
 async function main() {
-    const cacheFactoryOpts: CacheFactoryOpts = {
-        type: CacheType.FILE,
-        name: 'github',
-    };
-    const cache = CacheFactory.getInstance(cacheFactoryOpts);
+    const ghService = new GitHubService({ cacheType: CacheType.FILE });
 
-    const sample_key = 'reeee';
-    const sample = { ree: 'cola' };
-
-    await cache.set(sample_key, sample);
-
-    const ree = await cache.get(sample_key);
-    console.log(ree);
+    const orgInfo = await ghService.getOrgInfo('google');
+    console.log(orgInfo);
 }
 
 main();

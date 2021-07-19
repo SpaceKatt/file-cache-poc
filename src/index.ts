1e23d65104c654a1fe7d2e1b9ed9281f80287b3a
SpaceKatt <spacekattpoispin@gmail.com>
2021-07-18 21:33:23 -0700

    update main

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import { CacheType } from './interfaces';
 import { GitHubService } from './services';
+import { writeCompressedFile } from './utils/helpers';
 
 async function main() {
     const ghService = new GitHubService({ cacheType: CacheType.FILE });
 
     const orgInfo = await ghService.getOrgInfo('google');
     console.log(orgInfo);
+
+    const orgRepos = await ghService.getOrgRepos('google');
+
+    await writeCompressedFile(orgRepos, 'node_modules/.cache/test.json.gz');
 }
 
 main();

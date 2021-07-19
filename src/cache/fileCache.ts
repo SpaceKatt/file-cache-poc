6e016f1be8e021b1d1248ab2ed6064f4a7cd95a2
SpaceKatt <spacekattpoispin@gmail.com>
2021-07-18 17:03:01 -0700

    implement file cache

--- src/cache/fileCache.ts
+++ src/cache/fileCache.ts
@@ -1,17 +1,52 @@
+import * as fs from 'fs';
+import * as pfs from 'fs/promises';
+import * as path from 'path';
+
 import { Cache } from '../interfaces';
+import {
+    readCompressedFile,
+    utfToHex,
+    writeCompressedFile,
+} from '../utils/helpers';
 
 export class FileCache implements Cache {
-    private name: string;
+    static cachePathPrefix: string = 'node_modules/.cache';
+
+    private cachePath: string;
 
     constructor(name: string) {
-        this.name = name;
+        this.cachePath = path.join(FileCache.cachePathPrefix, name);
+
+        if (!fs.existsSync(FileCache.cachePathPrefix)) {
+            fs.mkdirSync(FileCache.cachePathPrefix);
+        }
+        if (!fs.existsSync(this.cachePath)) {
+            fs.mkdirSync(this.cachePath);
+        }
     }
 
-    get(key: string) {
-        throw new Error('Method not implemented.');
+    private getCachePath(key: string): string {
+        const encodedKey = `${utfToHex(key)}.json.gz`;
+        return path.join(this.cachePath, encodedKey);
     }
 
-    set(key: string, data: any): void {
-        throw new Error('Method not implemented.');
+    async get(key: string): Promise<any | undefined> {
+        const keyPath = this.getCachePath(key);
+        try {
+            const buff = await readCompressedFile(keyPath);
+            return JSON.parse(buff.toString());
+        } catch {
+            return undefined;
+        }
+    }
+
+    async set(key: string, data: any): Promise<void> {
+        const keyPath = this.getCachePath(key);
+        await writeCompressedFile(data, keyPath);
+    }
+
+    flush(): void {
+        fs.rmdirSync(this.cachePath, { recursive: true });
+        fs.mkdirSync(this.cachePath);
     }
 }

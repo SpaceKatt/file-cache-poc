5e8f36107167740b559c4af8ffc34959b69da5ec
SpaceKatt <spacekattpoispin@gmail.com>
2021-07-18 20:19:25 -0700

    implement getOrgInfo

--- src/services/githubService.ts
+++ src/services/githubService.ts
@@ -1,8 +1,15 @@
-import axios from 'axios';
-import { CacheFactory, CacheFactoryOpts } from '../factories';
 import { Cache, CacheType } from '../interfaces';
+import { CacheFactory, CacheFactoryOpts } from '../factories';
+import { getRequest } from '../utils/helpers';
+
+// import axios from 'axios';
 
 export interface OrgInfo {
+    name: string;
+    numberOfRepos: number;
+}
+
+export interface GitHubCachePayload {
     etag: string;
     data: any;
 }
@@ -12,7 +19,12 @@ export interface GitHubServiceOps {
 }
 
 export class GitHubService {
-    static cacheName = 'github';
+    static readonly apiHost = 'https://api.github.com';
+    static readonly cacheName = 'github';
+    static readonly defaultHeaders = {
+        Accept: 'application/vnd.github.v3+json',
+    };
+    static readonly orgRoute = '/orgs';
 
     private cache: Cache;
 
@@ -24,5 +36,44 @@ export class GitHubService {
         this.cache = CacheFactory.getInstance(cacheFactoryOpts);
     }
 
-    // getOrgInfo(): OrgInfo { }
+    private getOrgPath(org: string): string {
+        return `${GitHubService.apiHost}${GitHubService.orgRoute}/${org}`;
+    }
+
+    async getOrgInfo(org: string): Promise<OrgInfo> {
+        const etag = 'ree';
+        const data = 'cola';
+
+        const orgPath = this.getOrgPath(org);
+        // add etag here for caching
+        const cachedOrg = (await this.cache.get(orgPath)) as GitHubCachePayload;
+
+        const cachedHeaders = cachedOrg
+            ? {
+                  'If-None-Match': cachedOrg.etag,
+              }
+            : {};
+
+        const orgData = await getRequest(orgPath, {
+            ...cachedHeaders,
+            ...GitHubService.defaultHeaders,
+        });
+
+        // If undefined, then 304 indicates no update
+        if (!orgData) {
+            console.log('used cache');
+            return cachedOrg.data;
+        }
+        console.log('did NOT used cache');
+
+        const orgInfo: OrgInfo = {
+            name: org,
+            numberOfRepos: orgData.data.public_repos,
+        };
+
+        // don't await cache set
+        this.cache.set(orgPath, { etag: orgData.headers.etag, data: orgInfo });
+
+        return orgInfo;
+    }
 }

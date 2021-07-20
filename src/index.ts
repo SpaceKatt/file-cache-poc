ed0ca3dce91211dbd7c2490e70a8a385e313e2d4
SpaceKatt <spacekattpoispin@gmail.com>
2021-07-19 17:22:32 -0700

    remove silly code

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,6 @@ import { writeCompressedFile } from './utils/helpers';
 async function main() {
     const ghService = new GitHubService({ cacheType: CacheType.FILE });
 
-    const orgInfo = await ghService.getOrgInfo('google');
-    console.log(orgInfo);
-
     const orgRepos = await ghService.getOrgRepos('google');
 
     await writeCompressedFile(orgRepos, 'node_modules/.cache/test.json.gz');
